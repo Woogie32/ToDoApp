4e518efcb0b1a32d8978150952aa7d9bfcc5a367
Woogie <woogie.613@gmail.com>
2025-10-29 13:46:24 +0900

    #1 Create Views

---
+++ ToDoApp/Views/ListRowView.swift
@@ -0,0 +1,10 @@
+// ListRowView
+struct ListRowView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "checkmark.circle")
+            Text("This is the first item!")
+            Spacer()
+        }
+    }
+}

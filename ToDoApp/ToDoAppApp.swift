62f5391741c2f8aebd315a6d5c2c04c4054a85ac
Woogie <woogie.613@gmail.com>
2025-10-29 12:30:24 +0900

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by woogie on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
